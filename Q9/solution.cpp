b9afa56085c18dd57c7b9381d4048e74fc191d0d
Bharat chudasama <90544217+bharatchudasama@users.noreply.github.com>
2023-04-19 00:03:56 +0530

    Create solution.cpp

---
+++ Q9/solution.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string r1,r2;
+    cout<<"enter R1(in binary 8 bits):";cin>>r1;
+    cout<<"enter R2(in binary 8 bits):";cin>>r2;
+    string temp ="00000000";
+    for(int i = 0; i<8;i++){
+        if (r1[i]==r2[i] ){temp[i]='0';}
+        else{temp[i]='1';}
+    }
+    cout<<temp;
+
+    return 0;
+}
