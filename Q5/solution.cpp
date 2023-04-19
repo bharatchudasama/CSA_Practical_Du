c525f924051ba6b31e85d632e6c2efc6d6b60cf5
Bharat chudasama <90544217+bharatchudasama@users.noreply.github.com>
2023-04-19 08:16:49 +0530

    Update solution.cpp

--- Q5/solution.cpp
+++ Q5/solution.cpp
@@ -1,45 +1,28 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main() {
-    int value = 0;
-    bool isUp = true;
+  int value = 0;
+  int direction = 1; // 1 for up, -1 for down
 
-    // Prompt user to select up or down counter
-    cout << "Enter 'u' for up counter, 'd' for down counter: ";
-    char counterDirection;
-    cin >> counterDirection;
-    isUp = (counterDirection == 'u');
+  cout << "Enter the direction of the counter (1 for up, -1 for down): ";
+  cin >> direction;
 
-    // Loop through counter values and print them
-    for (int i = 0; i < 32; i++) {
-        // Convert value to binary string and print it
-        string binaryString = "";
-        int tempValue = value;
-        for (int j = 4; j >= 0; j--) {
-            if (tempValue >= pow(2, j)) {
-                binaryString += "1";
-                tempValue -= pow(2, j);
-            } else {
-                binaryString += "0";
-            }
-        }
-        cout << binaryString << endl;
-
-        // Update value based on counter direction
-        if (isUp) {
-            value++;
-            if (value >= 32) {
-                value = 0;
-            }
-        } else {
-            value--;
-            if (value < 0) {
-                value = 31;
-            }
-        }
+  cout << "Counter values:" << endl;
+  for (int i = 0; i < 32; i++) {
+    cout << value << endl;
+    if (direction == 1) {
+      value++;
+      if (value == 32) {
+        value = 0;
+      }
+    } else {
+      value--;
+      if (value == -1) {
+        value = 31;
+      }
     }
+  }
 
-    return 0;
+  return 0;
 }
