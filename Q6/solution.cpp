3ba003db28b00f74b5049876673e385cf3fea20e
Bharat chudasama <90544217+bharatchudasama@users.noreply.github.com>
2023-05-01 21:05:52 +0530

    Update solution.cpp

--- Q6/solution.cpp
+++ Q6/solution.cpp
@@ -1,8 +1,40 @@
 #include<iostream>
-#include<conio.h>
+
 #include<stdlib.h>
  
 using namespace std;
+int binaryToDecimal(int n)
+{
+	int num = n;
+	int dec_value = 0;
+	int base = 1;
+	int temp = num;
+	while (temp) {
+		int last_digit = temp % 10;
+		temp = temp / 10;
+
+		dec_value += last_digit * base;
+
+		base = base * 2;
+	}
+
+	return dec_value;
+}
+int decToBinary(int n)
+{
+
+    int binaryNum=0,temp=1;
+    while (n > 0) {
+        temp=temp*10 + (n % 2);
+        n = n / 2;
+    }
+    while (temp > 1) {
+        binaryNum=binaryNum*10 + (temp % 2);
+        temp = temp / 10;
+    }
+ 
+    return binaryNum;
+}
 int add(int x, int y)
 {
     int carry;
@@ -17,8 +49,12 @@ int add(int x, int y)
 int main()
 {
  
-    cout << "Enter the numbers to be added:";
+    cout << "Enter the number1 to be added:";
     int x, y;
-    cin >> x >> y;
-    cout << "The Summation is: " << add(x, y);
+    cin >> x 
+    cout << "Enter the number2 to be added:";
+    cin>> y;
+    int num1=binaryToDecimal(x);
+    int num2=binaryToDecimal(y);
+    cout << "The Summation is: " << decToBinary(add(num1, num2));
 }
